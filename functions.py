8b9cb58aa2db9e7f19dcb3f85463da15912e7f11
psihoshlem <yungync@gmail.com>
2023-11-03 19:22:01 +0300

    postgre разворачивается с расширением

---
+++ functions.py
@@ -0,0 +1,44 @@
+import psycopg2
+
+database_name = "test_db"
+user = "postgres"
+password = "1234"
+host = "localhost"
+port = "5432"
+
+conn = psycopg2.connect(
+    dbname=database_name,
+    user=user,
+    password=password,
+    host=host,
+    port=port
+)
+
+cur = conn.cursor()
+
+def get_pg_stat_activity():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM pg_stat_activity;")
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+
+    cur.close()
+
+    conn.close()
+
+
+def get_lwlock():
+    cur = conn.cursor()
+    cur.execute(
+            "SELECT count(*) FROM pg_stat_activity" +
+            " WHERE wait_event_type = 'LWLock';"
+        )
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    cur.close()
+    conn.close()
+
+get_lwlock()
+# get_pg_stat_activity()
