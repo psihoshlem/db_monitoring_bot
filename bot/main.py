262623ac77b605f93ef425fc6a4142daf25dd889
imadji <vip.obzor@bk.ru>
2023-11-05 01:30:58 +0300

    fix

--- bot/main.py
+++ bot/main.py
@@ -13,17 +13,20 @@ const_for_send_msg = True
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, text="Привет, <b>{0.first_name}</b>!".format(message.from_user), reply_markup=button_for_start(), parse_mode="HTML")
+    sent = bot.send_message(message.chat.id, f'Привет, <b>{message.from_user.first_name}</b>! Введите пароль для входа в систему', parse_mode="HTML")
+    bot.register_next_step_handler(sent, check_login)
 
 
 @bot.message_handler(content_types=['text'])
 def func(message):
-    if message.text == "Вход в систему":
-        button_for_auth(message)
-    elif message.text == "Cписок команд":
+    if message.text == "Cписок команд":
         show_all_commands(message)
     elif message.text == "Обновить БД":
         rebase_db(message)
+    elif message.text == "Выключить БД":
+        off_db(message)
+    elif message.text == "Включить БД":
+        on_db(message)
     else:
         bot.send_message(message.chat.id, text="На такую команду я не запрограммирован...")
 
@@ -32,21 +35,6 @@ def show_all_commands(message):
     button = telebot.types.InlineKeyboardButton('Показать все БД', callback_data='my_button')
     keyboard = telebot.types.InlineKeyboardMarkup().add(button)
     bot.send_message(message.chat.id, 'Cписок команд', reply_markup=keyboard)
-
-
-def button_for_start():
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Вход в систему")
-    markup.add(btn1)
-    return markup
-
-
-def button_for_auth(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    exit_btn = types.KeyboardButton('Выход')
-    markup.add(exit_btn)
-    sent = bot.send_message(message.chat.id, 'Введите пароль для входа в систему', reply_markup=markup)
-    bot.register_next_step_handler(sent, check_login)
 
 
 def warning_session_message(id, number):
@@ -64,10 +52,11 @@ def warning_long_query_message(id, pid, number, query):
 def check_login(message):
     if message.text == ADMIN_PASSWORD:
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Обновить БД")
-        btn2 = types.KeyboardButton("Cписок команд")
-        # bth3 = types.KeyboardButton("Спровоцировать ошибку")
-        markup.add(btn1, btn2)
+        btn1 = types.KeyboardButton("Выключить БД")
+        btn2 = types.KeyboardButton("Обновить БД")
+        btn3 = types.KeyboardButton("Включить БД")
+        btn4 = types.KeyboardButton("Cписок команд")
+        markup.add(btn1, btn2, btn3, btn4)
         bot.send_message(message.chat.id, 'Вход успешен', reply_markup=markup)
         write_admin(message.chat.id)
     else:
@@ -77,6 +66,16 @@ def check_login(message):
 
 def rebase_db(message):
     keyboard = create_inline_keyboard("show_bd_for_rebase")
+    bot.send_message(message.chat.id, 'Выберите db', reply_markup=keyboard)
+
+
+def off_db(message):
+    keyboard = create_inline_keyboard("show_bd_for_off")
+    bot.send_message(message.chat.id, 'Выберите db', reply_markup=keyboard)
+
+
+def on_db(message):
+    keyboard = create_inline_keyboard("show_bd_for_on")
     bot.send_message(message.chat.id, 'Выберите db', reply_markup=keyboard)
 
 
@@ -131,6 +130,22 @@ def process_callback_button(call):
     bot.send_photo(call.message.chat.id, photo=buf2, caption=db_name)
 
 
+@bot.callback_query_handler(func=lambda call: call.data.startswith('off_db'))
+def process_callback_button(call):
+    callback_data = call.data
+    db_name = callback_data.split('-')[1]
+    bot.send_message(call.message.chat.id, f'STOP DATABASE {db_name}')
+    # импорт функции для выключения бд
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('on_db'))
+def process_callback_button(call):
+    callback_data = call.data
+    db_name = callback_data.split('-')[1]
+    bot.send_message(call.message.chat.id, f'GO DATABASE {db_name}')
+    # импорт функции для включения бд
+
+
 @bot.callback_query_handler(func=lambda call: call.data.startswith('configuration'))
 def process_callback_button(call):
     buttons = [
@@ -169,8 +184,12 @@ def create_inline_keyboard(key_value):
     buttons = []
     if key_value == "show_bd_for_info":
         callback = "show_all_db"
-    else:
+    elif key_value == "show_bd_for_rebase":
         callback = "rebase_db"
+    elif key_value == "show_bd_for_off":
+        callback = "off_db"
+    elif key_value == "show_bd_for_on":
+        callback = "on_db"
     for db in dbs:
         callback_data = f"{callback}-{db}"
         btn = create_inline_button(db, callback_data)
